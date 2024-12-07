ccbcc5f3728410486f2ee4d7d20b475b0cc9823c
quinni1501 <huongquyen15012004@gmail.com>
2024-12-07 11:34:24 +0700

    ENTITY

---
+++ src/main/java/vn/iotstar/Demo4SpringbootApplication.java
@@ -0,0 +1,13 @@
+package vn.iotstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo4SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo4SpringbootApplication.class, args);
+	}
+
+}
